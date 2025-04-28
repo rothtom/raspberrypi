8c166778c4533fb621135b60c37b1d7a67095103
Tom Roth <t.roth11@icloud.com>
2025-04-28 17:20:00 +0200

    removed functions

--- led.py
+++ led.py
@@ -1,14 +1,12 @@
 from gpiozero import LED
 from time import sleep
 
-def main():
+
     
-    led = LED(17)
-    while 1:
-        led.on()
-        sleep(1)
-        led.off()
-        sleep(1)
+led = LED(17)
+while 1:
+    led.on()
+    sleep(1)
+    led.off()
+    sleep(1)
     
-if __name__ == "__main__":
-    main()
