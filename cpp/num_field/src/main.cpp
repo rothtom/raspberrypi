280e7310a65d456ec6469695fdf3bd493ad0c36f
Tom Roth <t.roth11@icloud.com>
2025-05-06 18:11:00 +0200

    started num field

--- cpp/num_field/src/main.cpp
+++ cpp/num_field/src/main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 #include <gpiod.hpp>
+#include <array>
+#include <vector>
 
 int main() {
     gpiod::chip chip("gpiochip0");
-    chip.get_line()
+    
+    std::array<std::array<char, 4>, 4> num_fields = {{
+        {'1', '2', '3', 'A'},
+        {'4', '5', '6', 'B'},
+        {'7', '8', '9', 'C'},
+        {'*', '0', '#', 'D'},
+    }};
+    
+    std::array<unsigned int, 8> line_offsets = {4, 17, 27, 22, 5, 6, 13, 19};
+    
+    std::array<gpiod::line, 8> lines;
+    for (int i=0; i<8; i++) {
+        lines[i] = chip.get_line(line_offsets[i]);
+        lines[i].request({
+            .consumer = "input " + i,
+            .request_type = gpiod::line_request::DIRECTION_INPUT
+        });
+    }
+
+
 }
